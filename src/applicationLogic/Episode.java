38bfb803d62f52650f2972c1d9289416fdc40749
Niek Flipse <npaflips@avans.nl>
2017-12-16 14:31:29 +0100

    Episode class now has object variables + constructor + getters

--- src/applicationLogic/Episode.java
+++ src/applicationLogic/Episode.java
@@ -1,4 +1,20 @@
 package applicationLogic;
 
-public class Episode {
+public class Episode extends Program {
+
+    private int episodeId;
+    private int episodeNumber;
+
+    public Episode(String title, double duration, int episodeNumber) {
+        super(title, duration);
+        this.episodeNumber = episodeNumber;
+    }
+
+    public int getEpisodeId() {
+        return episodeId;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
 }
