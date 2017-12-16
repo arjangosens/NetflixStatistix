99d7a0877cf0cbc95d8c1b6e039863b0ea32649d
Niek Flipse <npaflips@avans.nl>
2017-12-16 14:28:40 +0100

    Film class now has object variable + constructor + getters

--- src/applicationLogic/Film.java
+++ src/applicationLogic/Film.java
@@ -1,4 +1,32 @@
 package applicationLogic;
 
-public class Film {
+public class Film extends Program {
+
+    private int filmId;
+    private String genre;
+    private String language;
+    private int age;
+
+    public Film(String title, double duration, String genre, String language, int age) {
+        super(title, duration);
+        this.genre = genre;
+        this.language = language;
+        this.age = age;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
