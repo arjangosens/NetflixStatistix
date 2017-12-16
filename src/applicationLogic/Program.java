845822408b0d5fd8fd8964b335513056825ed6ee
Niek Flipse <npaflips@avans.nl>
2017-12-16 14:25:26 +0100

    Deleted unnecessery constructor

--- src/applicationLogic/Program.java
+++ src/applicationLogic/Program.java
@@ -5,10 +5,6 @@ public class Program {
     private String title;
     private double duration;
 
-    public Program(String title) {
-        this(title, 0);
-    }
-
     public Program(String title, double duration) {
         this.title = title;
         this.duration = duration;
