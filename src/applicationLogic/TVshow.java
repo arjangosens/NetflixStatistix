63441d9f2828a3d3bd22b3eef17f232e040cbb95
Niek Flipse <npaflips@avans.nl>
2017-12-16 14:36:00 +0100

    TVshow class now has object variables + constructor + getters

--- src/applicationLogic/TVshow.java
+++ src/applicationLogic/TVshow.java
@@ -1,4 +1,45 @@
 package applicationLogic;
 
+import java.util.ArrayList;
+
 public class TVshow {
+
+    private int tvshowId;
+    private String title;
+    private String genre;
+    private String language;
+    private int age;
+    private ArrayList<Episode> episodes;
+
+    public TVshow(String title, String genre, String language, int age, ArrayList<Episode> episodes) {
+        this.title = title;
+        this.genre = genre;
+        this.language = language;
+        this.age = age;
+        this.episodes = episodes;
+    }
+
+    public int getTvshowId() {
+        return tvshowId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ArrayList<Episode> getEpisodes() {
+        return episodes;
+    }
 }
