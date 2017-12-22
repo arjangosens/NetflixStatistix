6771dfc9aebe1e28e4b522121a3a02f163d02c59
Sam Glerum <samuelglerum@hotmail.com>
2017-12-22 17:05:52 +0100

    added object variables and constructors to Subscription and UserProfile

--- src/applicationLogic/Subscription.java
+++ src/applicationLogic/Subscription.java
@@ -1,4 +1,15 @@
 package applicationLogic;
 
 public class Subscription {
+    private String subName;
+    private String street;
+    private String houseNumber;
+    private String city;
+
+    public Subscription(String subName, String street, String houseNumber, String city) {
+        this.subName = subName;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.city = city;
+    }
 }
--- src/applicationLogic/UserProfile.java
+++ src/applicationLogic/UserProfile.java
@@ -1,4 +1,14 @@
 package applicationLogic;
 
 public class UserProfile {
+    private String profileName;
+    private int age;
+    private ViewBehaviour viewBehaviour;
+
+    public UserProfile(String profileName, int age) {
+        this.profileName = profileName;
+        this.age = age;
+        this.viewBehaviour = new ViewBehaviour();
+    }
+
 }
