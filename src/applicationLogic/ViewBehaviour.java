b111b03702ad30dee5ebcbbb56545f0a42ee3fa5
Sam Glerum <samuelglerum@hotmail.com>
2017-12-22 17:26:53 +0100

    added programs Getter, created add() and remove() method to add and remove programs

--- src/applicationLogic/ViewBehaviour.java
+++ src/applicationLogic/ViewBehaviour.java
@@ -8,4 +8,18 @@ public class ViewBehaviour {
     public ViewBehaviour() {
         this.programs = new HashMap<Program, Double>();
     }
+
+    public HashMap<Program, Double> getPrograms() {
+        return programs;
+    }
+
+    private void add(Program program, double progressPerct) {
+        if (!programs.containsKey(program)) {
+            programs.put(program, progressPerct);
+        }
+    }
+
+    private void remove(Program program) {
+        programs.remove(program);
+    }
 }
