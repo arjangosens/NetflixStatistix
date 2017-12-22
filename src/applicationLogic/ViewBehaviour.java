2ba99c7e339c5d6ed78b5dc2589d0695a85a6cf3
Sam Glerum <samuelglerum@hotmail.com>
2017-12-22 17:12:38 +0100

    added object variables to ViewBehaviour

--- src/applicationLogic/ViewBehaviour.java
+++ src/applicationLogic/ViewBehaviour.java
@@ -1,4 +1,11 @@
 package applicationLogic;
 
+import java.util.HashMap;
+
 public class ViewBehaviour {
+    private HashMap<Program, Double> programs;
+
+    public ViewBehaviour() {
+        this.programs = new HashMap<Program, Double>();
+    }
 }
