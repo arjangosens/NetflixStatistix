ea5d95ab6d9388765bf28d4c82685d6cb546e7ff
Niek Flipse <npaflips@avans.nl>
2017-12-24 19:44:36 +0100

    Created CardLayout

---
+++ src/userInterface/OverviewSwitchPanel.java
@@ -0,0 +1,38 @@
+package userInterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * This panel holds the overviews that are used in the CardLayout
+ */
+public class OverviewSwitchPanel extends JPanel {
+
+    private HomeScreen homeScreen;
+    private OverviewFilm overviewFilm;
+
+    private final static String HOMESCREENPANEL = "HomeScreenPanel";
+    private final static String OVERVIEWFILMPANEL = "OverviewFilmPanel";
+
+    public OverviewSwitchPanel() {
+        // Create the panels
+        homeScreen = new HomeScreen();
+        overviewFilm = new OverviewFilm();
+
+        // Set the actual CardLayout
+        this.setLayout(new CardLayout());
+
+        // Add the panels to the CardLayout
+        this.add(homeScreen, HOMESCREENPANEL);
+        this.add(overviewFilm, OVERVIEWFILMPANEL);
+    }
+
+    /**
+     * This method will switch between the panels that exists in the CardLayout indicated by the parameter
+     * @param panelIndicator The string that indicates the panel that the user will view
+     */
+    public void switchPanels(String panelIndicator) {
+        CardLayout cardLayout = (CardLayout) this.getLayout();
+        cardLayout.show(this, panelIndicator);
+    }
+}
