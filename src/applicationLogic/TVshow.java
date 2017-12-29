9e69e628b63c0fe97414121bc872eac35bf3d1c7
Niek Flipse <npaflips@avans.nl>
2017-12-29 16:38:36 +0100

    Changed the TVShow class constructor

--- src/applicationLogic/TVshow.java
+++ src/applicationLogic/TVshow.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 public class TVshow {
 
     private int tvshowId;
+    private int backup;
     private String title;
     private String genre;
     private String language;
     private int age;
     private ArrayList<Episode> episodes;
-    //TODO: Add object variable for relatedTO
 
-    public TVshow(String title, String genre, String language, int age, ArrayList<Episode> episodes) {
+    public TVshow(int tvshowId, int backup, String title, String genre, String language, int age) {
+        this.tvshowId = tvshowId;
+        this.backup = backup;
         this.title = title;
         this.genre = genre;
         this.language = language;
         this.age = age;
-        this.episodes = episodes;
     }
 
     public int getTvshowId() {
@@ -42,5 +43,22 @@ public class TVshow {
 
     public ArrayList<Episode> getEpisodes() {
         return episodes;
+    }
+
+    public void setEpisodes(ArrayList<Episode> episodes) {
+        this.episodes = episodes;
+    }
+
+    @Override
+    public String toString() {
+        return "TVshow{" +
+                "tvshowId=" + tvshowId +
+                ", backup=" + backup +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", language='" + language + '\'' +
+                ", age=" + age +
+                ", episodes=" + episodes +
+                '}';
     }
 }
