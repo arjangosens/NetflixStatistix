34a6988339b547af1611a3c7db09da2ce3f073e6
Sam Glerum <samuelglerum@hotmail.com>
2018-01-02 22:42:06 +0100

    created getAll() method for Subscription DAO which returns a Set of Subscriptions present in the database

--- src/database/SubscriptionDAO.java
+++ src/database/SubscriptionDAO.java
@@ -5,6 +5,8 @@ import applicationLogic.Subscription;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SubscriptionDAO {
 
@@ -16,7 +18,7 @@ public class SubscriptionDAO {
 
     /**
      * Get a single Subscription from the database
-     * @param ID the id of the Subscription that you would like to get
+     * @param id the id of the Subscription that you would like to get
      * @return Subscription The Subscription that is found in the database by the id
      */
     public Subscription getSubscription(int id) {
@@ -31,10 +33,10 @@ public class SubscriptionDAO {
             String query = "SELECT * FROM Subscription WHERE subscriptionId = " + id;
 
             // Create statement used to execute the query
-            Statement stamement = connection.createStatement();
+            Statement statement = connection.createStatement();
 
             // Execute the query. After executing a Resultset will be stored in this variable
-            ResultSet resultSet = stamement.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery(query);
 
             while (resultSet.next()) {
                 int subscriptionId = resultSet.getInt("subscriptionId");
@@ -60,4 +62,46 @@ public class SubscriptionDAO {
 
         return subscription;
     }
+
+    public Set getAll() {
+        Set<Subscription> subscriptionSet = new HashSet<Subscription>();
+        Connection connection = null;
+
+        try {
+            // Create connection with database
+            connection = databaseConnector.getConnection();
+
+            // Form SQL query to search for Subscriptions
+            String query = "SELECT * FROM Subscription";
+
+            // Create statement used to execute the query
+            Statement statement = connection.createStatement();
+
+            // Execute the query. After executing a Resultset will be stored in this variable
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int subscriptionId = resultSet.getInt("subscriptionId");
+                String nameSubscriber = resultSet.getString("nameSubscriber");
+                String streetName = resultSet.getString("streetName");
+                String houseNumber = resultSet.getString("houseNumber");
+                String city = resultSet.getString("city");
+
+                subscriptionSet.add(new Subscription(subscriptionId, nameSubscriber, streetName, houseNumber, city));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return subscriptionSet;
+    }
+
+    
 }
