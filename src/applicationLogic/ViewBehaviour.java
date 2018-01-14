38e2875081d83ee0340dfb29389644fbff3e572e
Arjan Gosens <arjangosens@hotmail.com>
2018-01-14 15:57:51 +0100

    Emptied 'ViewBehaviour'

--- src/applicationLogic/ViewBehaviour.java
+++ src/applicationLogic/ViewBehaviour.java
@@ -3,21 +3,5 @@ package applicationLogic;
 import java.util.HashMap;
 
 public class ViewBehaviour {
-    private HashMap<Program, Double> programs;
-
-    public ViewBehaviour() {
-        this.programs = new HashMap<Program, Double>();
-    }
-
-    public HashMap<Program, Double> getPrograms() {
-        return programs;
-    }
-
-    private void add(Program program, double progressPerct) {
-            programs.put(program, progressPerct);
-    }
-
-    private void remove(Program program) {
-        programs.remove(program);
-    }
+    
 }
