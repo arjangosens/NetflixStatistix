af948c673ea1cff20851f9b73c6d5a80f6887008
Niek Flipse <npaflips@avans.nl>
2018-01-14 14:00:25 +0100

    Deleted a duplicate constuctor

--- src/applicationLogic/UserProfile.java
+++ src/applicationLogic/UserProfile.java
@@ -6,12 +6,12 @@ public class UserProfile {
     private int age;
     private ViewBehaviour viewBehaviour;
 
-    public UserProfile(int subscriptionId, String profileName, int age) {
-        this.subscriptionId = subscriptionId;
-        this.profileName = profileName;
-        this.age = age;
-        this.viewBehaviour = new ViewBehaviour();
-    }
+//    public UserProfile(int subscriptionId, String profileName, int age) {
+//        this.subscriptionId = subscriptionId;
+//        this.profileName = profileName;
+//        this.age = age;
+//        this.viewBehaviour = new ViewBehaviour();
+//    }
 
     public UserProfile(int subId, String profileName, int age) {
         this.subId = subId;
