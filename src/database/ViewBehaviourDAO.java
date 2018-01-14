c26efb174689a27815990c5956b98e2be0e06dc7
Arjan Gosens <arjangosens@hotmail.com>
2018-01-14 14:22:46 +0100

    Added object variables and constructor to 'ViewBehaviourDAO'

--- src/database/ViewBehaviourDAO.java
+++ src/database/ViewBehaviourDAO.java
@@ -1,4 +1,9 @@
 package database;
 
 public class ViewBehaviourDAO {
+    private DatabaseConnector databaseConnector;
+
+    public ViewBehaviourDAO(DatabaseConnector databaseConnector) {
+        this.databaseConnector = databaseConnector;
+    }
 }
