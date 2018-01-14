f4478710d9fa6535b1c2333d9503284e430abaff
Arjan Gosens <arjangosens@hotmail.com>
2018-01-14 16:23:16 +0100

    Added getViewBehaviour() to 'ViewBehaviourDAO'

--- src/database/ViewBehaviourDAO.java
+++ src/database/ViewBehaviourDAO.java
@@ -1,9 +1,61 @@
 package database;
 
+import applicationLogic.ViewBehaviour;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ViewBehaviourDAO {
     private DatabaseConnector databaseConnector;
 
     public ViewBehaviourDAO(DatabaseConnector databaseConnector) {
         this.databaseConnector = databaseConnector;
+    }
+
+    /**
+     * Get a single ViewBehaviour from the database
+     *
+     * @param viewBehaviourId The ViewBehaviour that is found in the database by the viewBehaviourId
+     * @return ViewBehaviour the ViewBehaviour that is found in the database
+     */
+    public ViewBehaviour getViewBehaviour(int viewBehaviourId) {
+        ViewBehaviour viewBehaviour = null;
+        Connection connection = null;
+
+        try {
+            // Create connection with database
+            connection = databaseConnector.getConnection();
+
+            // Form SQL query to search for ViewBehaviour
+            String query = "SELECT * FROM ViewBehaviour WHERE viewBehaviourId = " + viewBehaviourId;
+
+            // Create statement used to execute the query
+            Statement statement = connection.createStatement();
+
+            // Execute the query. After executing a Resultset will be stored in this variable
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                viewBehaviourId = resultSet.getInt("viewBehaviourId");
+                int profileId = resultSet.getInt("profileId");
+                int programId = resultSet.getInt("programId");
+                double progressPerct = resultSet.getDouble("progressPerct");
+
+                viewBehaviour = new ViewBehaviour(viewBehaviourId, profileId, programId, progressPerct);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return viewBehaviour;
     }
 }
