31a5dbd715e3452adcebda06968935d1d89e39ee
Arjan Gosens <arjangosens@hotmail.com>
2018-01-15 13:32:15 +0100

    Added tests for Userprofile.deleteViewBehaviour()

--- Tests/applicationLogic/UserProfileTest.java
+++ Tests/applicationLogic/UserProfileTest.java
@@ -3,9 +3,10 @@ package applicationLogic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserProfileTest {
+
+    // Unit tests for addViewBehaviour()
+
     @Test
     void testAddViewBehaviourWithNewViewBehaviour() {
         //Arange
@@ -20,7 +21,7 @@ class UserProfileTest {
 
     @Test
     void testAddViewBehaviourWithExistingViewBehaviour() {
-        //Arange
+        //Arrange
         UserProfile userProfile = new UserProfile(1, 1, "Iemand", 18);
         ViewBehaviour viewBehaviour = new ViewBehaviour(1, 1, 1, 50);
         ViewBehaviour viewBehaviour1 = new ViewBehaviour(1, 1, 1, 50);
@@ -45,4 +46,46 @@ class UserProfileTest {
         Assertions.assertEquals(1, returnvalue);
     }
 
+    // Unit tests for deleteViewBehaviour()
+
+    @Test
+    void testDeleteViewBehaviourWithSameVariableForViewBehaviour() {
+        //Arrange
+        UserProfile userProfile = new UserProfile(1, 1, "Iemand", 18);
+        ViewBehaviour viewBehaviour = new ViewBehaviour(1, 1, 1, 50);
+        userProfile.addViewBehaviour(viewBehaviour);
+        //Act
+        userProfile.deleteViewBehaviour(viewBehaviour);
+        int returnvalue = userProfile.getViewBehaviourArrayList().size();
+        //Assert
+        Assertions.assertEquals(0, returnvalue);
+    }
+
+    @Test
+    void testDeleteViewBehaviourWithOtherViewBehaviour() {
+        //Arrange
+        UserProfile userProfile = new UserProfile(1, 1, "Iemand", 18);
+        ViewBehaviour viewBehaviour = new ViewBehaviour(1, 1, 1, 50);
+        ViewBehaviour otherViewBehaviour = new ViewBehaviour(2, 1, 2, 75);
+        userProfile.addViewBehaviour(viewBehaviour);
+        //Act
+        userProfile.deleteViewBehaviour(otherViewBehaviour);
+        int returnvalue = userProfile.getViewBehaviourArrayList().size();
+        //Assert
+        Assertions.assertEquals(1, returnvalue);
+    }
+
+    @Test
+    void testDeleteViewBehaviourWithOtherViewBehaviourWithSameValues() {
+        //Arrange
+        UserProfile userProfile = new UserProfile(1, 1, "Iemand", 18);
+        ViewBehaviour viewBehaviour = new ViewBehaviour(1, 1, 1, 50);
+        ViewBehaviour otherViewBehaviour = new ViewBehaviour(1, 1, 1, 50);
+        userProfile.addViewBehaviour(viewBehaviour);
+        //Act
+        userProfile.deleteViewBehaviour(otherViewBehaviour);
+        int returnvalue = userProfile.getViewBehaviourArrayList().size();
+        //Assert
+        Assertions.assertEquals(0, returnvalue);
+    }
 }
