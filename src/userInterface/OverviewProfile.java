b054a100862412053478b85c1823722a47bfd670
Unknown <samuelglerum@hotmail.com>
2018-01-15 04:09:54 +0100

    Created createProfileDialog() method
    Shows a dialog with input fields name and age, so user can add profiles. It's not working yet, clonfict with FK_Constraint

--- src/userInterface/OverviewProfile.java
+++ src/userInterface/OverviewProfile.java
@@ -2,6 +2,7 @@ package userInterface;
 
 import applicationLogic.*;
 import database.DatabaseConnector;
+import database.ProfileDAO;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -142,6 +143,52 @@ public class OverviewProfile extends JPanel implements Overview {
         }
     }
 
+    private void createProfileDialog() {
+        boolean isRunning = true;
+
+        // Create input fields for entering new profile data (Name and age)
+        JTextField profileName = new JTextField(40);
+        JTextField age = new JTextField(3);
+
+        // Create input panel for entering profile data
+        JPanel inputPanel = new JPanel();
+        // Set the layout to BoxLayout and let it the inputboxes align vertically
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+
+        // Create labels to placed above the corresponding textfield and add them to the inputPanel
+        inputPanel.add(new JLabel("Name: "));
+        inputPanel.add(profileName);
+        inputPanel.add(new JLabel("Age: "));
+        inputPanel.add(age);
+
+
+        // Create ProfileDao to enter data into the database
+        ProfileDAO profileDao = new ProfileDAO(new DatabaseConnector());
+        // The while loop is used to show the inputfields again in case of wrongly entered data
+        while (isRunning) {
+            try {
+                // Show input fields to user where he can enter his profile information
+                int n = JOptionPane.showConfirmDialog(null, inputPanel, "Enter your information", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+                // If user presses cancel or exit button, close dialog
+                if (n == JOptionPane.CANCEL_OPTION || n == JOptionPane.CLOSED_OPTION) {
+                    isRunning = false;
+                }
+                // Check if all fields have been filled in
+                if (profileName.getText().length() > 0 && profileName.getText().length() <= 40 && age.getText().length() > 0 && age.getText().length() <= 3) {
+
+                    // Call the insert() method, which inserts the data into the UserProfile table in the database
+                    profileDao.insert(subscriptionDropDown.getSelectedIndex(), profileName.getText(), Integer.parseInt(age.getText()));
+                    isRunning = false;
+                } else {
+                    // If the fields are empty, show an error message
+                    JOptionPane.showMessageDialog(inputPanel, "These fields cannot be empty");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void createComponents() {
         JLabel subscriptionDropDownLabel = new JLabel("Select Subscription:");
@@ -170,82 +217,13 @@ public class OverviewProfile extends JPanel implements Overview {
         /**
          * TODO: Create DAO for profiles and convert the commented out code to fit profiles instead of subscriptions
          */
-//        // Create DAO for getting all the registered subscriptions
-//        SubscriptionDAO getSubs = new SubscriptionDAO(new DatabaseConnector());
-//        // Create Set to store subscriptions
-//        Set<Subscription> setOfSubs = new HashSet<Subscription>();
-//        // Get all subscriptions from the database and add them to the set
-//        setOfSubs.addAll(getSubs.getAll());
-//
-//        // Create Arraylist to store all subscription ID's from the setOfSubs
-//        List<Integer> subIDs = new ArrayList<>();
-//
-//        // Loop through the setOfSubs and add all found subscriptionID's to the arrayList of subID's
-//        for (Subscription sub : setOfSubs) {
-//            subIDs.add(sub.getSubscriptionId());
-//        }
-//
-//        // Sort all subID's from smallest to greatest
-//        Collections.sort(subIDs);
-//
-//        // Loop through the arrayList of subID's, put each found SubID in the dropdown menu
-//        for (Integer subID : subIDs) {
-//            profileDropDown.addItem(subID);
-//        }
 
         // This button opens an input screen where users can make a new profile in the current subscription
         createNewProfileButton = new JButton("Create new profile");
         createNewProfileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                boolean isRunning = true;
-                // First the input fields are declared (profileName and profileAge)
-                JTextField profileName = new JTextField(40);
-                JTextField profileAge = new JTextField(40);
-                // A new input panel is created here
-                JPanel inputPanel = new JPanel();
-                // The layout is set to BoxLayout and the positioning is set to the Y_Axis, so the items will be stacked vertically
-                inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
-                // Labels are created and added to the input panel
-                inputPanel.add(new JLabel("Name:"));
-                inputPanel.add(profileName);
-                inputPanel.add(new JLabel("Age:"));
-                inputPanel.add(profileAge);
-                inputPanel.add(new JLabel("NOTE: The new profile will automatically be placed in the currently selected subscription." +
-                        "You can change the currently selected subscription in the \'Subscriptions\' overview."));
-                // The input panel is shown to the user
-
-                /**
-                 * TODO: Convert the commented out code to fit profiles instead of subscriptions
-                 */
-//                // Create SubDao to enter data into database
-//                SubscriptionDAO subDao = new SubscriptionDAO(new DatabaseConnector());
-//                // The while loop is used to show the inputfields again in case of wrongly entered data
-//                while (isRunning) {
-//                    try {
-//                        // Show input fields to user where he can enter his subscription information
-//                        int n = JOptionPane.showConfirmDialog(null, inputPanel, "Enter your information", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-//                        // If user presses cancel or exit button, close dialog
-//                        if (n == JOptionPane.CANCEL_OPTION || n == JOptionPane.CLOSED_OPTION) {
-//                            isRunning = false;
-//                            break;
-//                        }
-//                        // Check if all fields have been filled in
-//                        if (profileName.getText().length() > 0 && streetName.getText().length() > 0 && houseNumber.getText().length() > 0 && houseNumber.getText().length() <= 5 && city.getText().length() > 0) {
-//                            // Call the insert() method, which inserts the data into the Subscription table in the database
-//                            subDao.insert(profileName.getText(), streetName.getText(), houseNumber.getText(), city.getText());
-//                            isRunning = false;
-//                            // Check if the entered houseNumber doesn't exceed the limit. If it does, show an error message
-//                        } else if (houseNumber.getText().length() > 5) {
-//                            JOptionPane.showMessageDialog(inputPanel, "The housenumber can only be 5 characters long");
-//                        } else {
-//                            // If the fields are empty, show an error message
-//                            JOptionPane.showMessageDialog(inputPanel, "These fields cannot be empty");
-//                        }
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
+                createProfileDialog();
             }
         });
 
